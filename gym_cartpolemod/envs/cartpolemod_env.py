8a76fc68154b7ed87d92d2dd849434ad0bb0ed8c
Yifan Lin <39802487+ivanlin9522@users.noreply.github.com>
2023-04-11 20:33:38 -0400

    Update cartpolemod_env.py

--- gym_cartpolemod/envs/cartpolemod_env.py
+++ gym_cartpolemod/envs/cartpolemod_env.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 """
 Classic cart-pole system implemented by Rich Sutton et al.
 Copied from http://incompleteideas.net/sutton/book/code/pole.c
 permalink: https://perma.cc/C9ZM-652R
+Modified by Aaditya Ravindran to include friction and random sensor & actuator noise
 """
+
 import math
 from typing import Optional, Union
 
@@ -13,102 +16,63 @@ from gym import logger, spaces
 from gym.envs.classic_control import utils
 from gym.error import DependencyNotInstalled
 
+class CartPoleModEnv(gym.Env):
+	metadata = {
+			'render.modes': ['human', 'rgb_array'],
+			'video.frames_per_second' : 50
+	}
 
-class CartPoleLightEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
-    """
-    ### Description
-    This environment corresponds to the version of the cart-pole problem described by Barto, Sutton, and Anderson in
-    ["Neuronlike Adaptive Elements That Can Solve Difficult Learning Control Problem"](https://ieeexplore.ieee.org/document/6313077).
-    A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track.
-    The pendulum is placed upright on the cart and the goal is to balance the pole by applying forces
-     in the left and right direction on the cart.
-    ### Action Space
-    The action is a `ndarray` with shape `(1,)` which can take values `{0, 1}` indicating the direction
-     of the fixed force the cart is pushed with.
-    | Num | Action                 |
-    |-----|------------------------|
-    | 0   | Push cart to the left  |
-    | 1   | Push cart to the right |
-    **Note**: The velocity that is reduced or increased by the applied force is not fixed and it depends on the angle
-     the pole is pointing. The center of gravity of the pole varies the amount of energy needed to move the cart underneath it
-    ### Observation Space
-    The observation is a `ndarray` with shape `(4,)` with the values corresponding to the following positions and velocities:
-    | Num | Observation           | Min                 | Max               |
-    |-----|-----------------------|---------------------|-------------------|
-    | 0   | Cart Position         | -4.8                | 4.8               |
-    | 1   | Cart Velocity         | -Inf                | Inf               |
-    | 2   | Pole Angle            | ~ -0.418 rad (-24°) | ~ 0.418 rad (24°) |
-    | 3   | Pole Angular Velocity | -Inf                | Inf               |
-    **Note:** While the ranges above denote the possible values for observation space of each element,
-        it is not reflective of the allowed values of the state space in an unterminated episode. Particularly:
-    -  The cart x-position (index 0) can be take values between `(-4.8, 4.8)`, but the episode terminates
-       if the cart leaves the `(-2.4, 2.4)` range.
-    -  The pole angle can be observed between  `(-.418, .418)` radians (or **±24°**), but the episode terminates
-       if the pole angle is not in the range `(-.2095, .2095)` (or **±12°**)
-    ### Rewards
-    Since the goal is to keep the pole upright for as long as possible, a reward of `+1` for every step taken,
-    including the termination step, is allotted. The threshold for rewards is 475 for v1.
-    ### Starting State
-    All observations are assigned a uniformly random value in `(-0.05, 0.05)`
-    ### Episode End
-    The episode ends if any one of the following occurs:
-    1. Termination: Pole Angle is greater than ±12°
-    2. Termination: Cart Position is greater than ±2.4 (center of the cart reaches the edge of the display)
-    3. Truncation: Episode length is greater than 500 (200 for v0)
-    ### Arguments
-    ```
-    gym.make('CartPole-v1')
-    ```
-    No additional arguments are currently supported.
-    """
-
-    metadata = {
-        "render_modes": ["human", "rgb_array"],
-        "render_fps": 50,
-    }
-
-    def __init__(self, render_mode: Optional[str] = None):
-        self.gravity = 9.8
-        self.masscart = 1.0
-        self.masspole = 0.1 / 2
-        self.total_mass = self.masspole + self.masscart
-        self.length = 0.5  # actually half the pole's length
-        self.polemass_length = self.masspole * self.length
-        self.force_mag = 10.0
-        self.tau = 0.02  # seconds between state updates
+	def __init__(self,case=1):
+		self.__version__ = "0.2.0"
+		self.gravity = 9.8
+		self.masscart = 1.0
+		self.masspole = 0.1
+		self.total_mass = (self.masspole + self.masscart)
+		self.length = 0.5 # actually half the pole's length
+		self.polemass_length = (self.masspole * self.length)
+		self._seed()
+        if case == 2: # light 
+            self.masspole /= 2
+        elif case == 3: # heavy
+            self.masspole *= 2
+        elif case == 4: # long
+            self.length *= 2
+        elif case == 5: # short
+            self.length /= 2
+        elif case == 6: # soft
+            self.gravity /= 5
+        elif case == 7: # strong
+            self.gravity *= 5
+          
+		self.force_mag = 10.0
+		self.tau = 0.02  # seconds between state updates
         self.kinematics_integrator = "euler"
 
-        # Angle at which to fail the episode
-        self.theta_threshold_radians = 12 * 2 * math.pi / 360
-        self.x_threshold = 2.4
+		# Angle at which to fail the episode
+		self.theta_threshold_radians = 12 * 2 * math.pi / 360
+		self.x_threshold = 2.4
 
-        # Angle limit set to 2 * theta_threshold_radians so failing observation
-        # is still within bounds.
-        high = np.array(
-            [
-                self.x_threshold * 2,
-                np.finfo(np.float32).max,
-                self.theta_threshold_radians * 2,
-                np.finfo(np.float32).max,
-            ],
-            dtype=np.float32,
-        )
+		# Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
+		high = np.array([
+			self.x_threshold * 2,
+			np.finfo(np.float32).max,
+			self.theta_threshold_radians * 2,
+			np.finfo(np.float32).max])
 
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(-high, high, dtype=np.float32)
+		self.action_space = spaces.Discrete(2) 
+		self.observation_space = spaces.Box(-high, high, dtype=np.float32)
 
-        self.render_mode = render_mode
-
+		self.render_mode = render_mode
         self.screen_width = 600
         self.screen_height = 400
         self.screen = None
         self.clock = None
         self.isopen = True
         self.state = None
-
         self.steps_beyond_terminated = None
 
-    def step(self, action):
+
+    def _step(self, action):
         err_msg = f"{action!r} ({type(action)}) invalid"
         assert self.action_space.contains(action), err_msg
         assert self.state is not None, "Call reset before using step method."
@@ -168,7 +132,7 @@ class CartPoleLightEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
             self.render()
         return np.array(self.state, dtype=np.float32), reward, terminated, False, {}
 
-    def reset(
+    def _reset(
         self,
         *,
         seed: Optional[int] = None,
@@ -187,7 +151,7 @@ class CartPoleLightEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
             self.render()
         return np.array(self.state, dtype=np.float32), {}
 
-    def render(self):
+    def _render(self):
         if self.render_mode is None:
             gym.logger.warn(
                 "You are calling render method without specifying any render mode. "
@@ -284,7 +248,7 @@ class CartPoleLightEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
                 np.array(pygame.surfarray.pixels3d(self.screen)), axes=(1, 0, 2)
             )
 
-    def close(self):
+    def _close(self):
         if self.screen is not None:
             import pygame
 
